c0670ccf8c804693bcc0b11798148b6b6e5cdae5
manuelaguilar5219 <manuelaguilar5219@my.whccd.edu>
2024-11-13 22:35:40 -0800

    function that accepts a number from 0-100

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ myAverage = myAverage / myGrades.length;
 
 console.log("My grade average is " + myAverage);
 
+function validateNumber(input) {
+    if (typeof input !== 'number' || input < 0 || input > 100) {
+        return 'Input must be a number between 0 and 100';
+    }
+    return 'Input is valid';
+}
+
+console.log(validateNumber(50));  // Output: Input is valid
+console.log(validateNumber(101)); // Output: Input must be a number between 0 and 100
+
